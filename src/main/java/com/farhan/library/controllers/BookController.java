f3ee97fdd84080f873a69971b7631382714b0eb6
farhanqinoyz <farhanqinoyz@gmail.com>
2021-07-15 18:53:51 +0700

    Update Book API Find by name

--- src/main/java/com/farhan/library/controllers/BookController.java
+++ src/main/java/com/farhan/library/controllers/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
@@ -19,14 +20,14 @@ public class BookController {
     BookRepository bookRepository;
 
     @GetMapping("/load")
-    public Object loadBookPageable() {
+    public Object loadBookPageable(@RequestParam(required = false) String keyword) {
 
         Response response = new Response();
 
         try {
             List<Map<String, Object>> datas = new ArrayList<>();
             Map<String, Object> data = new HashMap<>();
-            List<Books> books = bookRepository.findAll();
+            List<Books> books = keyword != null ? bookRepository.getBooksByKeyword(keyword) : bookRepository.findAll();
             for(Books book : books){
                 Map<String, Object> loop = new HashMap<>();
                 loop.put("bookId", book.getBookId());
--- src/main/java/com/farhan/library/repositories/BookRepository.java
+++ src/main/java/com/farhan/library/repositories/BookRepository.java
@@ -2,7 +2,14 @@ package com.farhan.library.repositories;
 
 import com.farhan.library.models.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Books, Long> {
-
+    @Query(value = "SELECT * FROM books \n" +
+            "WHERE LOWER(books.title) LIKE LOWER(CONCAT('%',:keyword,'%')) " +
+            "ORDER BY title ASC", nativeQuery = true)
+    List<Books> getBooksByKeyword(@Param("keyword") String keyword);
 }
